01ea8ee48b5c001b5fa14b613c57b9277c21db32
TeaDoan <TeaDoan@github.com>
2018-06-09 17:11:07 -0600

    Initial Commit

---
+++ 2ndRedditSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2ndRedditSearch
+//
+//  Created by Thao Doan on 6/9/18.
+//  Copyright © 2018 Thao Doan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2ndRedditSearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  2ndRedditSearch
+//
+//  Created by Thao Doan on 6/9/18.
+//  Copyright © 2018 Thao Doan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
